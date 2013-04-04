4b4863b9a8c1d247995603a8ee6ec25dcc11bece
Daniel Pepin <dpepin@digitalbungalow.com>
2013-04-03 21:42:18 -0400

    Updated site.

--- js/djp.js
+++ js/djp.js
@@ -1,22 +1,50 @@
-(function ($) {
+(function($) {
+
+  $(document).ready(function() {
+    djp.attachClickEvents();
+    djp.showTheThingWithTheThing();
+  });
 
   /**
-   * On click, show the graph thingy.
+   * Main site object to contain functions/variables.
    */
-  $('.view-graph').click(function (e) {
-
-    e.preventDefault();
-    $('#graph-container').animate({height: 180}, 1500);
+  var djp = {
 
     /**
-     * Loop through and set bar widths.
+     * Displays the necessary site things.
      */
-    $.each($('#graph-container .progress .bar'), function (i, val) {
-      var width = $(val).width();
-      $(val).width(0);
-      $(val).delay((i + 1) * 250).animate({width: width}, 1500);
-    });
+    showTheThingWithTheThing: function() {
+      $('.hero-unit').delay(250).animate({opacity: 1}, 1000);
+      $('#footer').delay(500).animate({opacity: 1}, 500, function() {
+        // Sexy list animation for the footer.
+        $('#footer li').each(function(i) {
+          $(this).delay(i * 500).animate({marginTop: 0, opacity: 1}, 500);
+        });
+      });
 
-  });
+    },
+
+    /**
+     * Attach any click events we need.
+     */
+    attachClickEvents: function() {
+
+      // Visualize graph click event.
+      $('.view-graph').click(function(e) {
+
+        e.preventDefault();
+        $('#graph-container').animate({height: 180}, 1500);
+
+        // Loop through progress bars to animate them.
+        $.each($('#graph-container .progress .bar'), function(i, val) {
+          var width = $(val).width();
+          $(val).width(0);
+          $(val).delay((i + 1) * 250).animate({width: width}, 1500);
+        });
+
+      });
+    }
+
+  }
 
 }(jQuery));
